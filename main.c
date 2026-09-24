f40bd7be8302e350f8333524b405f4a4d10f37fc
AlphaNow <simon.palade37@gmail.com>
2026-09-24 15:26:34 +0200

    Add a main.c please collabore on it

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(void) {
+    printf("bonjour");
+    return 67;
+}
